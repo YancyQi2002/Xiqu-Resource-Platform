156be11686c1930e6144bbc33646a0ae21538b00
YancyQi2002 <xiaoyixiu2002@qq.com>
2022-03-14 14:30:19 +0800

    feat: :sparkles: initial commit

---
+++ .commitlintrc.js
@@ -0,0 +1,61 @@
+/** @type {import('cz-git').UserConfig} */
+module.exports = {
+  rules: {
+    // @see: https://commitlint.js.org/#/reference-rules
+  },
+  prompt: {
+    messages: {
+      type: "选择你的提交类型   | Select the type of change that you\'re committing:",
+      scope: "选择一个模块范围(可选) | Denote the SCOPE of this change (optional)",
+      customScope: "自定义修改模块名 | Denote the SCOPE of this change:",
+      subject: "简短说明 | Write a SHORT, IMPERATIVE tense description of the change:\n",
+      body: '详细说明(可选) 使用"|"可换行 \n  Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+      breaking: '非兼容性说明(可选) 使用"|"可换行 | List any BREAKING CHANGES (optional):\n',
+      footerPrefixsSelect: "选择关联issue前缀 | Select the ISSUES type of changeList by this change (optional):",
+      customFooterPrefixs: "输入自定义issue前缀 | Input ISSUES Prefix:",
+      footer: "列举关联issue (可选) 例如: #31, #I3244  List any ISSUES CLOSED by this change (optional) :\n",
+      confirmCommit: "是否提交或修改commit | Are you sure you want to proceed with the commit above?"
+    },
+    types: [
+      {value: 'feat',     name: 'feat:     ✨ 新增功能 | A new feature', emoji: ":sparkles:"},
+      {value: 'fix',      name: 'fix:      🐛 修复缺陷 | A bug fix', emoji: ":bug:"},
+      {value: 'docs',     name: 'docs:     📝 文档更新 | Documentation only changes', emoji: ":memo:"},
+      {value: 'style',    name: 'style:    💄 代码格式 | Changes that do not affect the meaning of the code', emoji: ":lipstick:" },
+      {value: 'refactor', name: 'refactor: ♻️ 代码重构 | A code change that neither fixes a bug nor adds a feature', emoji: ":recycle:"},
+      {value: 'perf',     name: 'perf:     ⚡️ 性能提升 | A code change that improves performance', emoji: ":zap:"},
+      {value: 'test',     name: 'test:     ✅ 测试相关 | Adding missing tests or correcting existing tests', emoji: ":white_check_mark:"},
+      {value: 'build',    name: 'build:    🏗️ 构建相关 | Changes that affect the build system or external dependencies', emoji: ":building_construction:"},
+      {value: 'ci',       name: 'ci:       💚 持续集成 | Changes to our CI configuration files and scripts', emoji: ":green_heart:"},
+      {value: 'revert',   name: 'revert:   ⏪️ 回退代码 | Revert to a commit', emoji: ":rewind:"},
+      {value: 'chore',    name: 'chore:    🔨 其他修改 | Other changes that do not modify src or test files', emoji: ":hammer:"},
+    ],
+    useEmoji: true,
+    scopes: [],
+    allowCustomScopes: true,
+    allowEmptyScopes: true,
+    customScopesAlign: "bottom",
+    customScopesAlias: "custom",
+    emptyScopesAlias: "empty",
+    allowBreakingChanges: ['feat', 'fix'],
+    upperCaseSubject: false,
+    breaklineChar: "|",
+    skipQuestions: [],
+    issuePrefixs: [
+      // 如果使用 gitee 作为开发管理
+      { value: "link", name: "link:     将任务状态更改为进行中"},
+      { value: "closed", name: "closed:   ISSUES 已经解决" }
+    ],
+    customIssuePrefixsAlign: "top",
+    emptyIssuePrefixsAlias: "skip",
+    customIssuePrefixsAlias: "custom",
+    confirmColorize: true,
+    maxHeaderLength: Infinity,
+    maxSubjectLength: Infinity,
+    minSubjectLength: 0,
+    scopeOverrides: undefined,
+    defaultBody: "",
+    defaultIssues: "",
+    defaultScope: "",
+    defaultSubject: ""
+  }
+}
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,62 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use tauri::{ Menu, Submenu, MenuItem, CustomMenuItem };
+
+// // 创建一个 Rust 命令 tauri::Manager
+// #[tauri::command]
+// fn close_splashscreen(window: tauri::Window) {
+//   // 关闭启动视图
+//   if let Some(splashscreen) = window.get_window("splashscreen") {
+//     splashscreen.close().unwrap();
+//   }
+//   // 展示主视图
+//   window.get_window("main").unwrap().show().unwrap();
+// }
+
+fn main() {
+  // 应用菜单
+  let submenu_gear = Submenu::new(
+    "Gear",
+    Menu::new()
+      .add_native_item(MenuItem::Copy)
+      .add_native_item(MenuItem::Paste)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Zoom)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Hide)
+      .add_native_item(MenuItem::CloseWindow)
+      .add_native_item(MenuItem::Quit)
+  );
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let submenu_customer = Submenu::new(
+    "Customer",
+    Menu::new()
+      .add_item(close)
+      .add_item(quit)
+  );
+  let menus = Menu::new()
+    .add_submenu(submenu_gear)
+    .add_submenu(submenu_customer);
+
+  tauri::Builder::default()
+    // 添加菜单
+    .menu(menus)
+    // 监听自定义菜单事件
+    .on_menu_event(|event| match event.menu_item_id() {
+      "quit" => {
+        std::process::exit(0);
+      }
+      "close" => {
+        event.window().close().unwrap();
+      }
+      _ => {}
+    })
+    // 注册命令
+    // .invoke_handler(tauri::generate_handler![close_splashscreen])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}

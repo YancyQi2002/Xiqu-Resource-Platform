e67bb7668e1df24335bb15bdfa8f5e7d96aea25e
YancyQi2002 <xiaoyixiu2002@qq.com>
2022-03-14 20:20:39 +0800

    feat: :sparkles: add Axios

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import './assets/css/index.css'
 
 import { createApp } from 'vue'
 
+import Axios from 'axios'
+
 import App from './App.vue'
 import router from './router/index'
 
 const app = createApp(App)
+app.config.globalProperties.$axios = Axios // 注入 axios
 app.use(router)
 app.mount('#app')

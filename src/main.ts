ebacaa8959175aa27be4e7d3250020a50dbcfe45
YancyQi2002 <xiaoyixiu2002@qq.com>
2022-03-15 11:03:12 +0800

    feat: :sparkles: add pinia

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import './assets/css/index.css'
 import { createApp } from 'vue'
 
 import Axios from 'axios'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router/index'
 
+const pinia = createPinia()
+
 const app = createApp(App)
 app.config.globalProperties.$axios = Axios // 注入 axios
-app.use(router)
+app.use(router).use(pinia)
 app.mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,19 @@
 <template>
   <img alt="Vue logo" src="../assets/logo.png" />
+  count: {{ countStore.num }}
+  <button class="w-24 bg-cyan-300 border-2 rounded-full" @click="countStore.increment()">+1</button>
+  refCount: {{ refCount.num }}
+  <button class="w-24 bg-cyan-300 border-2 rounded-full" @click="refCount.num.value++">+1</button>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts" setup>
+import { storeToRefs } from 'pinia'
 import HelloWorld from '../components/HelloWorld.vue'
+import useCountStore from '../store/module/useCountStore'
+
+const countStore = useCountStore()
+const refCount = storeToRefs(countStore)
 </script>
 
 <style scoped></style>

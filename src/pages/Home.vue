c0a22c54fe2435153c0109511dadcdc701adfc4f
YancyQi2002 <xiaoyixiu2002@qq.com>
2022-03-15 15:50:09 +0800

    feat: :sparkles: add element-plus auto-import

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,6 +4,9 @@
   <button class="w-24 bg-cyan-300 border-2 rounded-full" @click="countStore.increment()">+1</button>
   refCount: {{ refCount.num }}
   <button class="w-24 bg-cyan-300 border-2 rounded-full" @click="refCount.num.value++">+1</button>
+  <div class="my-2">
+    <el-button>Element-plus is OK!!</el-button>
+  </div>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 import * as path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
@@ -14,5 +17,13 @@ export default defineConfig({
       '@': resolve('./src')
     }
   },
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    })
+  ]
 })

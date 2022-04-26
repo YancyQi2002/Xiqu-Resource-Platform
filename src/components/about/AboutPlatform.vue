9a150ca05b43974177ce541bbccf1326cde3585a
YancyQi2002 <xiaoyixiu2002@qq.com>
2022-04-26 17:55:53 +0800

    chore: :hammer: update-version-to-convert-licence-to-AGPL-3.0-or-later
    Version update
    Change scrollbar style
    Change splash screen
    Change about page
    License switch to AGPL
    v3

--- src/components/about/AboutPlatform.vue
+++ src/components/about/AboutPlatform.vue
@@ -12,7 +12,7 @@
             <dl>
                 <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">Why was this platform created?</dt>
-                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">随着计算机网络技术的飞速发展,互联网已成为人们日常生活中不可或缺的一部分。一直以来大家使用较为传统的方式管理戏曲资源，这种管理方式存在着许多缺点。虽然也有一些戏曲资源网站，不过太过分散，这对于查找、更新和维护都带来了不少的困难。而使用新兴技术对各类戏曲资源进行管理，具有其它管理所无法比拟的优点。例如：查找方便、存储量大、及时更新优化等。这些优点能够极大地提高资源管理的效率。本平台是一个基于最新的 Web 框架技术开发的功能较为完备的戏曲资源平台。</dd>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">由于计算机技术的蓬勃发展，网络早已变成人类生活必要的组成部分。一直以来，人们在管理戏曲资源时一般都是采用的较为传统的模式，这种传统模式通常会存在着或多或少的缺点。虽然也有一些戏曲资源网站，不过太过分散，这对于使用者查找、开发者更新和维护数据都带来了不少的困难。而通过新兴信息技术对各类文化戏曲资源实施集中管理，则有着与其它管理所无法相比的优势。例如:查询方便、存贮容量大、及时更新优化等。上述特性能够明显提升资源管理的效能。本平台是一个基于最新的 Web 框架技术开发的功能较为完备的戏曲资源平台。</dd>
                 </div>
                 <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">What are the main techniques used?</dt>
@@ -21,8 +21,9 @@
                 <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">What licence does the platform use?</dt>
                 <dd class="flex items-start justify-start mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    <div class="py-2 pr-2">GPL-3.0-or-later</div>
-                    <img src="https://www.gnu.org/graphics/gplv3-or-later-sm.png">
+                    <div class="py-2 pr-2">AGPL-3.0-or-later</div>
+                    <img src="https://www.gnu.org/graphics/agplv3-with-text-162x68.png">
+                    <div class="py-2 pl-2">（ Current Version ）</div>
                 </dd>
                 </div>
             </dl>

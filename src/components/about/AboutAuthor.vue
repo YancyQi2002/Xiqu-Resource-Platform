1a62f4facbd0519db805b76359f03566751b7dbc
YancyQi2002 <xiaoyixiu2002@qq.com>
2022-05-30 21:00:28 +0800

    style: :lipstick: modify the style

--- src/components/about/AboutAuthor.vue
+++ src/components/about/AboutAuthor.vue
@@ -4,21 +4,21 @@
 
 <template>
     <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-        <div class="px-4 py-5 sm:px-6">
+        <div class="px-4 py-4 sm:px-6">
             <h3 class="text-lg leading-6 font-medium text-gray-900">关于作者</h3>
-            <p class="mt-1 max-w-2xl text-sm text-gray-500">About the Author</p>
+            <p class="mt-1 mb-0 max-w-2xl text-sm text-gray-500">About the Author</p>
         </div>
         <div class="border-t border-gray-200">
             <dl>
-                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <div class="bg-gray-50 px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">Author</dt>
                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Yancy Qi</dd>
                 </div>
-                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <div class="bg-white px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">E-mail</dt>
                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">xiaoyixiu2002@gmail.com</dd>
                 </div>
-                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <div class="bg-white px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">Blog</dt>
                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">yx-blog.vercel.app</dd>
                 </div>
